3c00189c6fe2c72d9a6d68ffad87a8a519f543a1
fungku <fungku@gmail.com>
2015-07-23 00:29:50 -0700

    Fix controller namespaces in routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,26 +4,26 @@
 // Wiki routes
 // ------------------------------------------------------------------------------
 $app->get('/', [
-    'uses' => 'Fungku\Kwiki\Http\Controllers\WikiController@index',
+    'uses' => 'WikiController@index',
     'as'   => 'wiki.index'
 ]);
 
 $app->get('{one}', [
-    'uses' => 'Fungku\Kwiki\Http\Controllers\WikiController@one',
+    'uses' => 'WikiController@one',
     'as'   => 'wiki.one'
 ]);
 
 $app->get('{one}/{two}', [
-    'uses' => 'Fungku\Kwiki\Http\Controllers\WikiController@two',
+    'uses' => 'WikiController@two',
     'as'   => 'wiki.two'
 ]);
 
 $app->get('{one}/{two}/{three}', [
-    'uses' => 'Fungku\Kwiki\Http\Controllers\WikiController@three',
+    'uses' => 'WikiController@three',
     'as'   => 'wiki.three'
 ]);
 
 $app->get('{one}/{two}/{three}/{four}', [
-    'uses' => 'Fungku\Kwiki\Http\Controllers\WikiController@four',
+    'uses' => 'WikiController@four',
     'as'   => 'wiki.four'
 ]);
